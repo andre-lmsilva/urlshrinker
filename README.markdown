cb00c571be5e0b5b9e8edb801a3dbcf43881ff1d
Andre Silva <andre.silva@hpe.com>
2021-08-18 14:20:57 +0100

    Basic project structure.

---
+++ README.markdown
@@ -0,0 +1,4 @@
+URL Shrinker
+================================================
+
+URL Shrinker is a small URL shortner web application composed by a set of Restful API endpoints and a small ReactJS single page application.
---
+++ src/main/java/com/nueda/assignment/urlshrinker/Application.java
@@ -0,0 +1,20 @@
+package com.nueda.assignment.urlshrinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class. Contains the entry point method for the entire application.
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Application entry point.
+     * @param args Not used for the application.
+     */
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
